77af5530053f5b4d224a914cf9c279b911319a2e
Ben Lleve <francisben.lleve@gmail.com>
2016-08-28 02:29:48 +0800

    Update args.py and added maxOfTwo.py

--- args.py
+++ args.py
@@ -2,12 +2,18 @@
 
 import sys
 
-def main():
- argv = []
- argv[:] = [ int(x) for x in sys.argv[1:] ]
- num1, num2 = argv[:2]
- return num1, num2
+def main(args):
+# argv = []
+ x = args + 1
+
+ if not len(sys.argv) > x:
+  d = {}
+  for a in sys.argv[1:x]:
+   d['{0}'.format(a)] = int(a)
+  return d
+# argv[:] = [ int(x) for x in sys.argv[1:] ]
+# num1, num2 = argv[:2]
+# return num1, num2
 
 if __name__ == "__main__":
- main()
-
+ main(0) 
---
+++ maxOfTwo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import args
+
+def maxi(args1, args2=0):
+ if args1 > args2:
+  return args1
+ else:
+  return args2
+
+if __name__ == '__main__':
+ if args.main(2) != None:
+  num1, num2 = args.main(2)
+  print( maxi(num1, num2) )
+ else:
+  print("More than 2 values entered. Only need 2.")
