573629ee61502d7cb805747b30da07ce4422416a
Ben Lleve <francisben.lleve@gmail.com>
2016-08-28 00:17:28 +0800

    first commit

---
+++ maxi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+def maxi(args1, args2=0):
+ if args1 > args2:
+  return args1
+ else:
+  return args2
+
+if __name__ == '__main__':
+ argv = [] 
+ argv[:] = [ int(x) for x in sys.argv[1:] ] 
+ num1, num2 = argv[:2]
+# print(type(argv[1:]))
+ print(maxi(num1, num2))
+# print(sys.argv[1:]) 
