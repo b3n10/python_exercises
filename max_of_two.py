55c9e6652e62c89aa979128e519123993dcb4d81
Ben Lleve <francisben.lleve@gmail.com>
2016-08-29 10:22:57 +0800

    Update max_of_two.py

--- max_of_two.py
+++ max_of_two.py
@@ -13,7 +13,7 @@ def maxi(args1, args2=0):
 if __name__ == '__main__':
  if args.main(2) != None:
   num1, num2 = args.main(2)
-  print(type(num1), ' ', type(num2))
+  #print(type(num1), ' ', type(num2))
   print( maxi(num1, num2) )
  else:
   print("More than 2 values entered. Only need 2.")
