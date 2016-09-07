f20d76f33702c7623040a3527c0751ee0c2ae3cf
Ben Lleve <francisben.lleve@gmail.com>
2016-09-07 09:47:32 +0800

    Add alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,16 @@
+#/usr/bin/python
+
+def main(s):
+ x, y, new_word = 0, 0, ''
+ while y + 1 < len(s):
+  #print(s[y] + " <= " + s[y+1] + " and " + str(y) + " + 1 < " + str(len(s)))
+  while s[y] <= s[y+1] and y + 1 <= len(s):
+   new_word += s[y]
+   if y + 2 >= len(s) and s[y+1] > s[y]:
+    new_word += s[y+1]
+   break
+  y += 1
+ print(new_word)
+
+if __name__ == "__main__":
+ main('ytsabcdefghij')
