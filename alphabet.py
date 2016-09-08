9831f94754165c645bfa6b34c3a06d01c497b671
Ben Lleve <francisben.lleve@gmail.com>
2016-09-08 09:17:00 +0800

    Update alphabet.py, GAME ON

--- alphabet.py
+++ alphabet.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python
 
 def main(s):
- x, y, new_word = 0, 0, ''
- while y + 1 < len(s):
-  #print(s[y] + " <= " + s[y+1] + " and " + str(y) + " + 1 < " + str(len(s)))
-  while s[y] <= s[y+1] and y + 1 <= len(s):
-   new_word += s[y]
-   if y + 2 >= len(s) and s[y+1] > s[y]:
-    new_word += s[y+1]
-   break
-  y += 1
- print(new_word)
+ pL, nW, nL = '','', []
+
+ for x in range(len(s)):
+  print('pL: ' + pL + ', s['+ str(x) + ']: ' + s[x] + ' = ' + nW)
+  if pL == '':
+   pL = s[x]
+   nW = pL
+  else:
+   if pL <= s[x]:
+    if nW == '':
+     nW += pL + s[x]
+    else:
+     nW += s[x]
+    pL = s[x]
+   elif s[x] < pL:
+    nL.append(nW)
+    nW = ''
+    pL = s[x]
+ 
+ if nW != '':
+  nL.append(nW)
+ 
+ print(nL)
+ print( max(nL, key=len) )
 
 if __name__ == "__main__":
- main('ytsabcdefghij')
+ main('ijeimcxqxxxhfvvsud')
