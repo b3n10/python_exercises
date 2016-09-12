ff9180eb54e551009d4bd711389a0d1480c1536b
Ben Lleve <francisben.lleve@gmail.com>
2016-09-12 08:52:23 +0800

    Add secret_number.py

---
+++ secret_number.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+'''
+Created on Sep 12, 2016 at 5:09AM
+
+Author: Francis Ben Lleve
+'''
+def main():
+    i, g, s, e = '', 50, 0, 100
+
+    while i != 'c':
+        #print('g: {0}, s: {1}, e: {2}'.format(str(g),str(s),str(e)))
+        print('Is your secret number ' + str(g) + '?')
+        i = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+        if i == 'h':
+            e = g
+            if e - s <= 5:
+                g = int(g - 1)
+            else:
+                g = s + int((e - s) / 2)
+                #e = int(g)
+        elif i == 'l':  
+            s = g
+            if e - s <= 2:
+                g = int(g + 1)
+            else:
+                g += int((e - s) / 2)
+        elif i == 'c':
+            print('I have guessed it correctly: ' + str(g))
+        else:
+            print('Incorrect input')
+
+if __name__ == '__main__':
+    main()
