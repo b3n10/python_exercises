7f3526f3ea7abc3f573d8ea3606b05a47e30a2fb
Ben Lleve <francisben.lleve@gmail.com>
2016-09-20 08:42:31 +0800

    Added gcdRecur.py

---
+++ gcdRecur.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def gcdRecur(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
+
+if __name__ == "__main__":
+  print(gcdRecur(17, 12))
