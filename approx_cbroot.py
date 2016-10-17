6444b6046297e9843b144ce3131ebca2efec6810
ben <francisben.lleve@gmail.com>
2016-10-18 07:29:10 +0800

    Add approx_sqroot.py and approx_cbroot.py

---
+++ approx_cbroot.py
@@ -0,0 +1,23 @@
+x = -27
+epsilon = 0.01
+numGuesses = 0
+
+if x < 0:
+    low = x
+else:
+    low = 0.0
+
+high = max(1.0, x)
+ans = (high + low)/2.0
+
+while abs(ans**3 - x) >= epsilon:
+    print('low =', low, 'high =', high, 'ans =', ans)
+    numGuesses += 1
+    if ans**3 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low)/2.0
+
+print('numGuesses =', numGuesses)
+print(ans, 'is close to cube root of', x)
